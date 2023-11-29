af1c115f1e463292d6165e71054787859fc5fed9
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-29 11:24:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 function App() {
   return (
     <>
-      <header>
-        <h1>Olá React! 😎</h1>
-        <hr />
-        <nav>
-          <a href="">Home</a>
-          <a href="">Produtos</a>
-          <a href="">Contato</a>
-        </nav>
-      </header>
       {/* Comentário no meio do JS
       É normal ficar entre {} nesta situação
      */}
