d60f54b17af17587d9cbafbe0c40b62e4c34c645
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-29 11:00:35 -0300

    comentário no jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
 function App() {
-  return <>
+  return <> 
     <header> 
     <h1>Olá React! 😎</h1>
   </header> 
+    {/* Comentário no meio do JS
+      É normal ficar entre {} nesta situação
+     */}
+
 
   <main>
     <h2>Teste</h2>
