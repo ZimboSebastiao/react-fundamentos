8c05942fde821cf14ba8e632bfad6da5f61cb16c
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-29 11:25:08 -0300

    Update Cabecalho.jsx

--- src/Cabecalho.jsx
+++ src/Cabecalho.jsx
@@ -1,5 +1,13 @@
 function Cabecalho() {
-    return (
-
-    );
+  return (
+    <header>
+      <h1>Olá React! 😎</h1>
+      <hr />
+      <nav>
+        <a href="">Home</a>
+        <a href="">Produtos</a>
+        <a href="">Contato</a>
+      </nav>
+    </header>
+  );
 }
