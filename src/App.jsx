16f3c7c4bba235f97f298867723e8c6602a1991f
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-29 11:38:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
 import Cabecalho from "./Cabecalho";
+import Conteudo from "./Conteudo";
 
 function App() {
   return (
     <>
       <Cabecalho />
-      <main>
-        <section>
-          <h2>Conteúdo da aplicação</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-            itaque minus atque odio quis quidem labore aliquam repellendus
-            architecto soluta temporibus error similique, rem cumque magnam
-            fugiat ducimus dignissimos animi.
-          </p>
-
-          <article>
-            <h3>Artigo 1</h3>
-            <p>Conteúdo do artigo1...</p>
-          </article>
-          <article>
-            <h3>Artigo 2</h3>
-            <p>Conteúdo do artigo...</p>
-          </article>
-          <article>
-            <h3>Artigo 3</h3>
-            <p>Conteúdo do artigo...</p>
-          </article>
-        </section>
-      </main>
-
+      <Conteudo />
       <footer>
         <h2> Rodapé da aplicação</h2>
       </footer>
