f6062e72269091a2f6936ab823e8c7968b7ca22e
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-29 11:38:01 -0300

    Create Conteudo.jsx

---
+++ src/Conteudo.jsx
@@ -0,0 +1,30 @@
+function Conteudo() {
+  return (
+    <main>
+      <section>
+        <h2>Conteúdo da aplicação</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+          itaque minus atque odio quis quidem labore aliquam repellendus
+          architecto soluta temporibus error similique, rem cumque magnam fugiat
+          ducimus dignissimos animi.
+        </p>
+
+        <article>
+          <h3>Artigo 1</h3>
+          <p>Conteúdo do artigo1...</p>
+        </article>
+        <article>
+          <h3>Artigo 2</h3>
+          <p>Conteúdo do artigo...</p>
+        </article>
+        <article>
+          <h3>Artigo 3</h3>
+          <p>Conteúdo do artigo...</p>
+        </article>
+      </section>
+    </main>
+  );
+}
+
+export default Conteudo;
