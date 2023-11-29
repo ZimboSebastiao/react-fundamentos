d9f6a86a4fb82b22260342372f0ceba7ee2a2b81
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-29 10:53:03 -0300

    uso de fragmento

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 
 function App() {
-  return <h1>Olá React! 😎</h1>
+  return <>
+    <header> 
+    <h1>Olá React! 😎</h1>
+  </header> 
+
+  <main>
+    <h2>Teste</h2>
+  </main>
+  </> 
 }
 
 export default App
