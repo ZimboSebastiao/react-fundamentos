d43ce881633fcb5d7ba315a6db2a50870bfb6dc2
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-30 09:41:00 -0300

    css inline e em uma constante

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -1,7 +1,14 @@
 function Rodape() {
+  const exemploEstilos = {
+    backgroundColor: "lightcyan",
+    padding: "3%",
+    color: "blue",
+    border: "solid 4px #de56",
+  };
+
   return (
-    <footer style={{ backgroundColor: "lightcyan", padding: "3%" }}>
-      <h2> Rodapé da aplicação</h2>
+    <footer style={exemploEstilos}>
+      <h2 style={{ textAlign: "center" }}> Rodapé da aplicação</h2>
     </footer>
   );
 }
