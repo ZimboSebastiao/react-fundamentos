0af132accc8c53acfef83cec4354e1eddc7194e1
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-11-30 10:01:47 -0300

    importação de css externo

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 // Importação das libs essenciais (React e ReactDOM)
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Importação do primeiro/principal componente, arquivo em que carregamos os elementos da página (como fosse um index/home)
-import App from './App.jsx'
-
+import App from "./App.jsx";
+import "./index.css";
 
 // Inicialização dos recursos do React, manipulando a div#root existente no index.html
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
-
+);
