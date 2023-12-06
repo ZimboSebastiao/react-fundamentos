7a610f8252370beb60dc35b316bc3cb513b10fb8
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-12-06 11:38:12 -0300

    Update Artigo.jsx

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,4 +1,41 @@
+import { useState } from "react";
 import styled from "styled-components";
+
+// Definindo props para o componente
+function Artigo({ categoria, titulo, preco }) {
+  const formatarPreco = (valor) => {
+    return valor.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+  };
+
+  const [cor, setCor] = useState("lavender");
+
+  const mudarCor = () => {
+    if (cor == "lavender") {
+      setCor("pink");
+    } else {
+      setCor("lavender");
+    }
+  };
+
+  return (
+    <StyledArtigo style={{ background: cor }}>
+      <div onClick={mudarCor}>
+        <h3>{categoria}</h3>
+        <p>
+          <b> Curso: </b>
+          {titulo}
+        </p>
+        <p>
+          <b>Preço: </b>
+          {formatarPreco(preco)}
+        </p>
+      </div>
+    </StyledArtigo>
+  );
+}
 
 const StyledArtigo = styled.article`
   background-color: lavender;
@@ -18,30 +55,5 @@ const StyledArtigo = styled.article`
     padding: 4px;
   }
 `;
-
-// Definindo props para o componente
-function Artigo({ categoria, titulo, preco }) {
-  const formatarPreco = (valor) => {
-    return valor.toLocaleString("pt-br", {
-      style: "currency",
-      currency: "BRL",
-    });
-  };
-  return (
-    <StyledArtigo>
-      <div>
-        <h3>{categoria}</h3>
-        <p>
-          <b>Curso: </b>
-          {titulo}
-        </p>
-        <p>
-          <b>Preço: </b>
-          {formatarPreco(preco)}
-        </p>
-      </div>
-    </StyledArtigo>
-  );
-}
 
 export default Artigo;
