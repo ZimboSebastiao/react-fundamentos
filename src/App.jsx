93a9781cacd21d9a26ecf64c14f44810468fc9e4
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-12-07 11:28:13 -0300

    uso de children

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Cabecalho from "./components/Cabecalho";
 import Rodape from "./components/Rodape";
+import Home from "./pages/Home";
+import Produtos from "./pages/Produtos";
+import Contato from "./pages/Contato";
+import Container from "./components/Container";
 
 function App() {
   return (
     <>
       <Cabecalho />
-
+      <Container>
+        <Home />
+        <Produtos />
+        <Contato />
+      </Container>
       <Rodape />
     </>
   );
