1be0b43abf7d7b3e8d4bacd440a677f075e74cc7
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-12-07 09:13:15 -0300

    utilizando states para filtrar

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -3,33 +3,20 @@ import Artigo from "./Artigo";
 import styled from "styled-components";
 import "../api/cursos.js";
 import cursos from "../api/cursos.js";
+import { useState } from "react";
 
-const StyledConteudo = styled.main`
-  width: 90vw;
-  margin: 1rem auto;
-  background-color: aliceblue;
-  padding: 1rem;
-  box-shadow: darkblue 0 0 1px inset;
-
-  h2,
-  p {
-    padding: 0.2rem 0;
-  }
-
-  @media screen and (min-width: 650px) {
-    .artigos {
-      display: flex;
-      justify-content: flex-start;
-      flex-wrap: wrap;
-
-      & article {
-        width: 48%;
-        margin: 1%;
-      }
-    }
-  }
-`;
 function Conteudo() {
+  // Gerenciador de states para mudança/filtro de categoria. Inicialmente começa como null pois ainda não temos uma escolha/seleção de categoria (aparece tudo)
+  const [categoria, setCategoria] = useState(null);
+
+  const aplicarFiltro = (event) => {
+    // Capturamos (apos o clique) o texto escrito em cada botão
+    const categoriaEscolhida = event.currentTarget.innerText;
+
+    // E em seguida passamos este texto para o state de categoria
+    setCategoria(categoriaEscolhida);
+  };
+
   return (
     <StyledConteudo>
       <h2>Conteúdo da aplicação</h2>
@@ -40,6 +27,18 @@ function Conteudo() {
         labore iste veniam possimus facere adipisci sit repellat, voluptate,
         expedita aspernatur.
       </p>
+
+      <div className="filtros">
+        <p>
+          <b>Filtar por: </b>
+          <button onClick={aplicarFiltro}>Front-End</button>
+          <button onClick={aplicarFiltro}>Back-End</button>
+          <button onClick={aplicarFiltro}>Design</button>
+        </p>
+        <p>
+          Você escolheu: <b>{categoria}</b>
+        </p>
+      </div>
 
       <div className="artigos">
         {cursos.map((curso) => {
@@ -56,5 +55,44 @@ function Conteudo() {
     </StyledConteudo>
   );
 }
+
+const StyledConteudo = styled.main`
+  width: 90vw;
+  margin: 1rem auto;
+  background-color: aliceblue;
+  padding: 1rem;
+  box-shadow: darkblue 0 0 1px inset;
+
+  h2,
+  p {
+    padding: 0.2rem 0;
+  }
+  .filtros {
+    margin: 1rem 0;
+    padding: 1rem 0;
+    border-top: solid 2px;
+    border-bottom: solid 2px;
+  }
+
+  .filtros button {
+    margin: 0.5rem;
+  }
+  button {
+    background-color: #728ea5;
+  }
+
+  @media screen and (min-width: 650px) {
+    .artigos {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+
+      & article {
+        width: 48%;
+        margin: 1%;
+      }
+    }
+  }
+`;
 
 export default Conteudo;
