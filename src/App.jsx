dbf489bceb02d335847ed5d80ea1b3b76fdfff4a
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-12-07 11:05:35 -0300

    create de pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import Cabecalho from "./components/Cabecalho";
-import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
 
 function App() {
   return (
     <>
       <Cabecalho />
-      <Conteudo />
+
       <Rodape />
     </>
   );
