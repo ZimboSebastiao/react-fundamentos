36551d7b3b52cdfa841693f939f10ed162327a32
Zimbo Sebastião <94145573+ZimboSebastiao@users.noreply.github.com>
2024-01-15 11:20:41 -0300

    Uso do useEffect

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,21 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Produtos() {
-
   // O state "produtos" é iniciado como um array vazio. Posteriormente (após o carregamento dos dados da api) ele será preenchido com os objetos/produtos.
-  const [produtos, setProdutos] =  useState([]);
+  const [produtos, setProdutos] = useState([]);
 
-  const carregarDados = async () => {
-    try {
-      const resposta = await fetch(`https://fakestoreapi.com/products`);
-     const dados = await resposta.json();
-     console.log(dados);
-    setProdutos(dados);
-    } catch (error){
-      console.error("Houve um erro: "+error)
-    }
-  }
-  carregarDados();
+  // Gerenciando efeito colateral dp componente para o carregamento dos da api
+
+  useEffect(() => {
+    const carregarDados = async () => {
+      try {
+        const resposta = await fetch(`https://fakestoreapi.com/products`);
+        const dados = await resposta.json();
+        console.log(dados);
+        setProdutos(dados);
+      } catch (error) {
+        console.error("Houve um erro: " + error);
+      }
+    };
+
+    carregarDados();
+  }, []);
+
   return <h2>Produtos</h2>;
 }
 
